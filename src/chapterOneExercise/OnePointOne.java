5e47422b1743c106aa056ed25ddae204e9c0cd5b
jdnew <623893416@qq.com>
2015-12-20 20:45:55 +0800

    提交习题1.1.1

---
+++ src/chapterOneExercise/OnePointOne.java
@@ -0,0 +1,25 @@
+package chapterOneExercise;
+
+public class OnePointOne {
+
+	public static void main(String[] args) {
+		onePointOnePointOne();
+
+	}
+
+	/**
+	 * 习题1.1.1 
+	 */
+	private static void onePointOnePointOne() {
+		int i = (0+15)/2;
+		double d = (0+15)/2;
+		char c = (0+15)/2;
+		System.out.println("int = " + i);
+		System.out.println("double = " + d);
+		System.out.println("char = " + c);
+		
+	}
+	
+	
+
+}
